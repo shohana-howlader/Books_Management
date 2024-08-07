9d95cd4b3b5f9f1eeb44159f4b95d4e1f2bf9fab
shohana-howlader <166001432+shohana-howlader@users.noreply.github.com>
2024-08-08 02:47:03 +0600

    Add project files.

---
+++ project/Models/ViewModels/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace project.Models.ViewModels
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
